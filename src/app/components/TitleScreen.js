e5d1e8f9959ac1c2d8319ea192ae4c8dc450f7c3
AdamCanard <azcunard@gmail.com>
2024-04-18 22:34:16 -0600

    first commit

---
+++ src/app/components/TitleScreen.js
@@ -0,0 +1,95 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+
+export default function TitleScreen() {
+  const outerRef = useRef(null);
+  const oneRef = useRef(null);
+  const twoRef = useRef(null);
+  const threeRef = useRef(null);
+  const fourRef = useRef(null);
+  const fiveRef = useRef(null);
+  // const [oneSwitch, setOne] = useState(false);
+  // const [twoSwitch, setTwo] = useState(false);
+  // const [threeSwitch, setThree] = useState(false);
+  // const [fourSwitch, setFour] = useState(false);
+  // const [fiveSwitch, setFive] = useState(false);
+
+  useEffect(() => {
+    var one = oneRef.current;
+    var two = twoRef.current;
+    var three = threeRef.current;
+    var four = fourRef.current;
+    var five = fiveRef.current;
+    var outer = outerRef.current;
+    outer.scrollIntoView();
+    one.onanimationstart = () => {
+      one.scrollIntoView({ block: "center", inline: "center" });
+    };
+
+    two.onanimationstart = () => {
+      console.log("2");
+      two.scrollIntoView({ block: "center", inline: "center" });
+    };
+    three.onanimationstart = () => {
+      console.log("3");
+      three.scrollIntoView({ block: "center", inline: "center" });
+    };
+    four.onanimationstart = () => {
+      console.log("4");
+      four.scrollIntoView({ block: "center", inline: "center" });
+    };
+    five.onanimationstart = () => {
+      console.log("5");
+      outer.scrollIntoView({ block: "center", inline: "center" });
+    };
+  }, []);
+
+  return (
+    <>
+      <div
+        ref={outerRef}
+        className="flex h-full w-full bg-[#9046CF] justify-center items-center overflow-hidden"
+      >
+        <div
+          ref={oneRef}
+          className="flex z-10 h-7/8 w-7/8 bg-[#AE4FD2] rounded-full justify-center items-center absolute animate-fall1"
+        ></div>
+
+        <div
+          ref={twoRef}
+          className="flex z-20 h-6/8 w-6/8 bg-[#CC59D2] rounded-full justify-center items-center absolute animate-fall2"
+        ></div>
+
+        <div
+          ref={threeRef}
+          className="flex z-30 h-5/8 w-5/8 bg-[#F487B6] rounded-full justify-center items-center absolute animate-fall3"
+        ></div>
+
+        <div
+          ref={fourRef}
+          className="flex z-40 h-4/8 w-4/8 bg-[#FDE12D] rounded-full justify-center items-center absolute animate-fall4"
+        ></div>
+
+        <div
+          ref={fiveRef}
+          className="flex z-50 h-3/8 w-3/8 justify-center items-center animate-fall5"
+        >
+          <h1 className="flex text-8xl text-center">CAN I BOTHER YOU?</h1>
+          {/* <h1 className="flex text-9xl text-center">C</h1>
+                  <h1 className="flex text-9xl text-center">A</h1>
+                  <h1 className="flex text-9xl text-center">N</h1>
+                  <h1 className="flex text-9xl text-center">I</h1>
+                  <h1 className="flex text-9xl text-center">B</h1>
+                  <h1 className="flex text-9xl text-center">O</h1>
+                  <h1 className="flex text-9xl text-center">T</h1>
+                  <h1 className="flex text-9xl text-center">H</h1>
+                  <h1 className="flex text-9xl text-center">E</h1>
+                  <h1 className="flex text-9xl text-center">R</h1>
+                  <h1 className="flex text-9xl text-center">Y</h1>
+                  <h1 className="flex text-9xl text-center">O</h1>
+                  <h1 className="flex text-9xl text-center">U</h1> */}
+        </div>
+      </div>
+    </>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,75 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        fall1: "fall1 .5s .5s ease-in both",
+        fall2: "fall2 .5s 1s ease-in both",
+        fall3: "fall3 .5s 1.5s ease-in both",
+        fall4: "fall4 .5s 2s ease-in both",
+        fall5: "fall5 .5s 2.5s ease-in both",
+      },
+      keyframes: {
+        fall1: {
+          from: {
+            transform: "translatey(-700px) translatex(-500px)",
+          },
+          to: {
+            transition: "all var(--spring-duration) var(--spring-easing)",
+            padding: ".4rem 0",
+            display: "flex",
+          },
+        },
+        fall2: {
+          from: {
+            transform: "translatey(-700px) translatex(500px)",
+          },
+          to: {
+            transition: "all var(--spring-duration) var(--spring-easing)",
+            padding: ".4rem 0",
+          },
+        },
+        fall3: {
+          from: {
+            transform: "translatey(500px) translatex(-500px)",
+          },
+          to: {
+            transition: "all var(--spring-duration) var(--spring-easing)",
+            padding: ".4rem 0",
+          },
+        },
+        fall4: {
+          from: {
+            transform: "translatey(450px) translatex(500px)",
+          },
+          to: {
+            transition: "all var(--spring-duration) var(--spring-easing)",
+            padding: ".4rem 0",
+          },
+        },
+        fall5: {
+          from: {
+            transform: "translatey(-500px)",
+          },
+          to: {
+            transition: "all var(--spring-duration) var(--spring-easing)",
+            padding: ".4rem 0",
+          },
+        },
+      },
+      height: {
+        "7/8": "87.5%",
+        "6/8": "75%",
+        "5/8": "62.5%",
+        "4/8": "50%",
+        "3/8": "37.5%",
+      },
+      width: {
+        "7/8": "87.5%",
+        "6/8": "75%",
+        "5/8": "62.5%",
+        "4/8": "50%",
+        "3/8": "37.5%",
+      },
     },
   },
   plugins: [],
